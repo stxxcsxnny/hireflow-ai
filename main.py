a4e4b66a7c36280b75af3592e8e230d1b7af5846
stxxcsxnny <sk2993130@gmail.com>
2026-09-13 04:57:23 +0530

    Connect frontend to live backend

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "https://hireflowsfrontend.vercel.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
